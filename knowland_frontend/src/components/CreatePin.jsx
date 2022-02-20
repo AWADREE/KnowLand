d54094ac36dd4982da0d7963fbcab3bcc638111b
AWADREE <62517784+AWADREE@users.noreply.github.com>
2022-02-20 05:56:45 +0200

    added some validation for the creatPin

--- knowland_frontend/src/components/CreatePin.jsx
+++ knowland_frontend/src/components/CreatePin.jsx
@@ -17,7 +17,11 @@ const CreatePin = ({ user }) => {
   const [wrongImageType, setWrongImageType] = useState(false);
 
   const navigate = useNavigate(); //using the user navigation hook
-
+  const validateFields = () => {
+    if (title && about && destination && imageAsset?._id && category) {
+      setFields(false);
+    }
+  };
   //uploading image to sanity db
   const uploadImage = (e) => {
     const { type, name } = e.target.files[0];
@@ -40,6 +44,8 @@ const CreatePin = ({ user }) => {
         .then((document) => {
           setImageAsset(document);
           setLoading(false);
+          validateFields();
+          setFields(false);
         })
         .catch((error) => {
           console.log("Image upload error", error);
@@ -80,20 +86,14 @@ const CreatePin = ({ user }) => {
     } else {
       setFields(true);
       //clear the fields after 2 seconds
-      setTimeout(() => {
-        setFields(false);
-      }, 2000);
+      // setTimeout(() => {
+      //   setFields(false);
+      // }, 5000);
     }
   };
 
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
-      {fields && (
-        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in ">
-          Please fill in all the fields
-        </p>
-      )}
-
       <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
         <div className=" bg-secondaryColor p-3 flex flex-0.7 w-full rounded-lg">
           <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
@@ -107,10 +107,7 @@ const CreatePin = ({ user }) => {
               </div>
             )}
             {!imageAsset ? (
-              <label
-                className="cursor-pointer bg-stone-50 p-5 rounded-xl"
-                style={{ maxWidth: "inherit" }}
-              >
+              <label className="cursor-pointer bg-stone-50 p-5 rounded-xl">
                 <div className="flex flex-col items-center justify-center h-full">
                   <div className="flex flex-col justify-center items-center">
                     <p className="font-bold text-2xl">
@@ -119,8 +116,7 @@ const CreatePin = ({ user }) => {
                     <p className="text-lg">Click to upload</p>
                   </div>
                   <p className="mt-32 text-gray-400 ">
-                    {" "}
-                    Use heigh-quality JPG, SVG, PNG or GIF less than 20 MB
+                    JPG, SVG, PNG, GIF or TIFF
                   </p>
                 </div>
                 <input
@@ -157,6 +153,7 @@ const CreatePin = ({ user }) => {
             value={title}
             onChange={(e) => {
               setTitle(e.target.value);
+              validateFields();
             }}
             placeholder="Add your title here"
             className="outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2"
@@ -176,6 +173,7 @@ const CreatePin = ({ user }) => {
             value={about}
             onChange={(e) => {
               setAbout(e.target.value);
+              validateFields();
             }}
             placeholder="What is your pin about"
             className="outline-none text-base sm:text-lg  border-b-2 border-gray-200 p-2"
@@ -185,6 +183,7 @@ const CreatePin = ({ user }) => {
             value={destination}
             onChange={(e) => {
               setDestination(e.target.value);
+              validateFields();
             }}
             placeholder="Add a destination link"
             className="outline-none text-base sm:text-lg  border-b-2 border-gray-200 p-2"
@@ -195,22 +194,33 @@ const CreatePin = ({ user }) => {
                 Chosse Pin Category
               </p>
               <select
-                onChange={(e) => setCategory(e.target.value)}
+                onChange={(e) => {
+                  setCategory(e.target.value);
+                  validateFields();
+                }}
                 className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
               >
                 <option value="other" className="bg-white">
                   Select Category
                 </option>
-                {categories.map((category) => (
+                {categories.map((category, index) => (
                   <option
                     className="text-base border-0 outline-none capitalize bg-white text-black"
                     value={category.name}
+                    key={index + 1}
                   >
                     {category.name}
                   </option>
                 ))}
               </select>
             </div>
+            {fields && (
+              <div className="flex justify-center mt-3">
+                <p className="text-red-600 mb-5 text-xl transition-all duration-150 ease-in ">
+                  Please fill in all the fields
+                </p>
+              </div>
+            )}
             <div className="flex justify-end items-end mt-5">
               <button
                 type="button"
